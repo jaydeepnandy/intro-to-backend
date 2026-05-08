a428dd810a45f162c3f33208d7b711e23b0921e1
jaydeepnandy <jaydeep.nandy@gmail.com>
2026-05-08 19:37:01 +0100

    Mongodb Connection

---
+++ backend/src/app.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+// Create an express application
+const app = express();
+
+export default app;
---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import app from "./app.js";
+
+// Load environment variables from .env file
+dotenv.config(
+    { path: "./.env" }
+);
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        
+        app.on("error", (error) => {
+            console.log("ERROR", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+
+    } catch(error) {
+        console.error("Failed to connect to MongoDB!", error);
+        process.exit(1);
+    }
+}
+
+startServer();
